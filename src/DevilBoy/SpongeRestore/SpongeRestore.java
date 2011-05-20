a5cad26b0658c0119b8317ff178af0f19aff62b5
Joshua Reetz <josh100288@jrtechsupport.com>
2011-05-20 00:49:01 -0600

    Added recipie and image of it.

--- src/DevilBoy/SpongeRestore/SpongeRestore.java
+++ src/DevilBoy/SpongeRestore/SpongeRestore.java
@@ -6,11 +6,13 @@ import java.util.HashMap;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Type;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -35,6 +37,13 @@ public class SpongeRestore extends JavaPlugin {
     	pm.registerEvent(Event.Type.BLOCK_PLACE, blockListener, Priority.Normal, this);
     	pm.registerEvent(Event.Type.BLOCK_FROMTO, blockListener, Priority.Normal, this);
     	pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener, Priority.Normal, this);
+    	
+    	//Adding sponge recipe
+    	ShapedRecipe spongerecipie = new ShapedRecipe(new ItemStack(19));
+        spongerecipie.shape("SXS","XSX","SXS");
+        spongerecipie.setIngredient('S', Material.SAND);
+        spongerecipie.setIngredient('X', Material.STRING);
+        getServer().addRecipe(spongerecipie);
 
         // EXAMPLE: Custom code, here we just output some info so we can check all is well
         PluginDescriptionFile pdfFile = this.getDescription();
